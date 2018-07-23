33731e53097481f7db47882aa52f06e8ffeaaccf
Matthew <mdwareing@gmail.com>
2018-07-23 17:10:33 +0100

    Enable to pull down tweets from twitter

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
-var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+const Twitter = require('twitter');
+const ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+require('dotenv').config()
 
-var toneAnalyzer = new ToneAnalyzerV3({
-  version: '2017-09-21',
-  username: '18274e70-6707-4969-8dbe-894b72b3964d',
-  password: 'edl0T1OeguXQ'
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-var text = 'Team, I know that times are tough! Product sales have been disappointing for the past three quarters. We have a competitive product, but we need to do a better job of selling it!'
+var params = {
+  q: '#nodejs',
+  count: 10,
+  result_type: 'recent',
+  lang: 'en'
+}
+client.get('search/tweets', params, function(err, data, response) {
+  var my_arr = []
+  if(!err){
+    for(let i = 0; i < data.statuses.length; i++) {
+      my_arr.push(data.statuses[i].text)
+    }
+  } else {
+    console.log(err);
+  }
+  console.log(my_arr)
+})
 
-var toneParams = {
+const toneAnalyzer = new ToneAnalyzerV3({
+  version: '2017-09-21',
+  username: process.env.TONEANALYZER_USERNAME,
+  password: process.env.TONEANALYZER_PASSWORD
+});
+
+const text = 'To Iranian President Rouhani: NEVER, EVER THREATEN THE UNITED STATES AGAIN OR YOU WILL SUFFER CONSEQUENCES THE LIKES OF WHICH FEW THROUGHOUT HISTORY HAVE EVER SUFFERED BEFORE. WE ARE NO LONGER A COUNTRY THAT WILL STAND FOR YOUR DEMENTED WORDS OF VIOLENCE & DEATH. BE CAUTIOUS!'
+
+const toneParams = {
   'tone_input': { 'text': text },
   'content_type': 'application/json'
 };
