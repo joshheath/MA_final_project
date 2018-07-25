0434018811f9b0e07f488cd7b4681dd59f62d39c
Nick <nicholasjohnrichardson@gmail.com>
2018-07-25 15:59:13 +0100

    refactored getTweets method out of index and added mockTwitter to mock twitter client

--- spec/TwitCallSpec.js
+++ spec/TwitCallSpec.js
@@ -2,17 +2,22 @@ describe("Twitter", function() {
 
   var twitcall;
   var mockTwitter;
+  var promisedData = [{trends: [{name: "#wednesdaywisdom"}]}];
 
   beforeEach(function() {
-    mockTwitter = { get: jasmine.createSpy('get') }
+    mockTwitter = { get: function() {
+                    return promisedData;
+                                  } };
     twitcall = new TwitCall(mockTwitter);
+
+    spyOn(mockTwitter, 'get')
   })
 
   describe("#getTrends", function() {
     it('calls the Twitter API', function() {
-      twitcall.getTrends()
+      twitcall.getTrends();
       expect(mockTwitter.get).toHaveBeenCalled();
-    })
-  })
+    });
+  });
 
-})
+});
--- src/TwitCall.js
+++ src/TwitCall.js
@@ -14,10 +14,12 @@ TwitCall.prototype.getTrends = function () {
   return new Promise(resolve => {
     this._client.get(`https://api.twitter.com/1.1/trends/place.json?id=44418`, function(err, data, response) {
       if(!err) {
+        console.log(data[0].trends[0].name)
         var top10Trends = []
         for(let i = 0; i < 10; i++) {
           top10Trends.push(data[0].trends[i].name)
         }
+        console.log(top10Trends)
         resolve(top10Trends);
       } else {
         console.log(err);
@@ -25,6 +27,29 @@ TwitCall.prototype.getTrends = function () {
     })
   })
 };
+
+TwitCall.prototype.getTweets = function(trend) {
+  return new Promise(function(resolve) {
+    const params = {
+      q: `${trend}`,
+      count: 10,
+      lang: 'en'
+    }
+    const trendHash = {trend: "", tweets: []}
+    client.get('search/tweets', params, function(err, data, response) {
+      trendHash.trend = trend;
+      if (!err) {
+        data.statuses.forEach(function(tweet) {
+          trendHash.tweets.push(tweet.text)
+        })
+      } else {
+        console.log(err)
+      }
+      resolve(trendHash);
+    })
+  })
+}
+
 
 var twitcall = new TwitCall();
 
