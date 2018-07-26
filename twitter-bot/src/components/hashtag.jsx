294d89db435515e66882d5f99992c6e290f8fb23
Matthew <mdwareing@gmail.com>
2018-07-26 11:10:27 +0100

    Rename welcome files to hashtag

--- twitter-bot/src/components/hashtag.jsx
+++ twitter-bot/src/components/hashtag.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class Welcome extends React.Component {
+export default class Hashtag extends React.Component {
     render() {
         return (
             <div>Hello world</div>
