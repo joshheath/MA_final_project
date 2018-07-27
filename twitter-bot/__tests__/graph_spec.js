1f51fd7674755aede16dacb54efddc5281983341
Chris Russell <chris.john.russell@gmail.com>
2018-07-27 14:55:06 +0100

    Add react-d3

---
+++ twitter-bot/__tests__/graph_spec.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Hashtag from '../src/components/hashtag.jsx';
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('Graph', () => {
+  it('Graph renders data', () => {
+    const data = [
+      {quarter: 1, earnings: 65000},
+      {quarter: 2, earnings: 16500},
+      {quarter: 3, earnings: 14250},
+      {quarter: 4, earnings: 19000}
+    ];
+
+    const graph = Enzyme.shallow(<Graph visData={data}/>);
+  });
+});
--- twitter-bot/src/App.js
+++ twitter-bot/src/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Hashtag from './components/hashtag.jsx';
+import { VictoryBar, VictoryChart, VictoryAxis } from 'victory';
 
+const data = [
+  {quarter: 1, earnings: 100},
+  {quarter: 2, earnings: 100},
+  {quarter: 3, earnings: 100},
+  {quarter: 4, earnings: 100}
+];
 
 class App extends Component {
   render() {
@@ -15,6 +22,26 @@ class App extends Component {
         </p>
         <div>
           <Hashtag hashtags={["matt", "wareing"]}/>
+        </div>
+        <div>
+        <div>
+        <VictoryChart
+        domainPadding={20}
+        >
+        <VictoryAxis
+        tickValues={[1, 2, 3, 4]}
+        tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
+        />
+        <VictoryAxis
+        dependentAxis
+        tickFormat={(x) => (`${x / 100}`)}
+        />
+            <VictoryBar
+            data={data}
+            x="quarter"
+            y="earnings" />
+            </VictoryChart>
+            </div>
         </div>
       </div>
     );
