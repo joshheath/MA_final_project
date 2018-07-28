2643f280992f2a24725bdfb32e930d6d178493c5
Josh Heath <37807868+joshheath@users.noreply.github.com>
2018-07-28 13:36:55 +0100

    lint spec folder contents

--- twitter-bot/spec/naturalLanguageCall.test.js
+++ twitter-bot/spec/naturalLanguageCall.test.js
@@ -1,26 +1,26 @@
 const NaturalLanguageCall = require('../src/NaturalLanguageCall')
 
-  var mockNaturalLanguage;
-  var naturallanguagecall;
+var mockNaturalLanguage
+var naturallanguagecall
 
-  describe('#analyze language', function(){
-    it('understands the language used in tweets', function(done){
-      mockNaturalLanguage = {
-        analyze: function(parameters, callback){
-          callback(null, {sentiment: 'positive'})
-        },
-      },
-      naturallanguagecall = new NaturalLanguageCall(mockNaturalLanguage);
+describe('#analyze language', function () {
+  it('understands the language used in tweets', function (done) {
+    mockNaturalLanguage = {
+      analyze: function (parameters, callback) {
+        callback(null, {sentiment: 'positive'})
+      }
+    }
+    naturallanguagecall = new NaturalLanguageCall(mockNaturalLanguage)
 
-      naturallanguagecall.analyzeLanguage().then(function(language){
-        expect(language).toEqual({ sentiment: 'positive'})
-      })
-      done();
+    naturallanguagecall.analyzeLanguage().then(function (language) {
+      expect(language).toEqual({sentiment: 'positive'})
     })
+    done()
+  })
 
-    it('calls the language analyzer', function() {
-      spyOn(mockNaturalLanguage, 'analyze')
-      naturallanguagecall.analyzeLanguage();
-      expect(mockNaturalLanguage.analyze).toHaveBeenCalled();
-    });
-  });
+  it('calls the language analyzer', function () {
+    spyOn(mockNaturalLanguage, 'analyze')
+    naturallanguagecall.analyzeLanguage()
+    expect(mockNaturalLanguage.analyze).toHaveBeenCalled()
+  })
+})
--- twitter-bot/spec/toneAnalyzerCall.test.js
+++ twitter-bot/spec/toneAnalyzerCall.test.js
@@ -1,26 +1,26 @@
 const ToneAnalyzerCall = require('../src/ToneAnalyzerCall')
 
-  var mockToneAnalyzer;
-  var toneanalyzercall;
+var mockToneAnalyzer
+var toneanalyzercall
 
-  describe("#analyzeSentiment", function(){
-    it('assesses the sentiment of tweets', function(done){
-      mockToneAnalyzer = {
-        tone: function(toneParams, callback){
-          callback(null, "negative")
-        },
-      };
-      toneanalyzercall = new ToneAnalyzerCall(mockToneAnalyzer);
+describe('#analyzeSentiment', function () {
+  it('assesses the sentiment of tweets', function (done) {
+    mockToneAnalyzer = {
+      tone: function (toneParams, callback) {
+        callback(null, 'negative')
+      }
+    }
+    toneanalyzercall = new ToneAnalyzerCall(mockToneAnalyzer)
 
-      toneanalyzercall.analyzeSentiment().then(function(sentiment){
-        expect(sentiment).toEqual({tones: ["negative"]})
-      });
-      done();
+    toneanalyzercall.analyzeSentiment().then(function (sentiment) {
+      expect(sentiment).toEqual({tones: ['negative']})
     })
+    done()
+  })
 
-    it('calls the Twitter API', function() {
-      spyOn(mockToneAnalyzer, 'tone')
-      toneanalyzercall.analyzeSentiment();
-      expect(mockToneAnalyzer.tone).toHaveBeenCalled();
-    });
-  });
+  it('calls the Twitter API', function () {
+    spyOn(mockToneAnalyzer, 'tone')
+    toneanalyzercall.analyzeSentiment()
+    expect(mockToneAnalyzer.tone).toHaveBeenCalled()
+  })
+})
