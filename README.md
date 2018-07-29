d89d97c2a41b8db9933f5048304b3e44ddadca6b
Matthew <mdwareing@gmail.com>
2018-07-29 16:34:05 +0100

    Refactor project file structure

--- README.md
+++ README.md
@@ -1,18 +1,4 @@
-# Makers Final Project - Twitter Sentiment Bot
-
-This is our final project at Makers. Our team of 4 (Nick, Matt, Chris and Josh) have made an app that uses tone and sentiment analysis software to return insights on tweets and hashtags returned by Twitter's API.  
-  
-This information is presented both via automated Language Reports tweeted out via a [twitter-bot](https://twitter.com/BotAnalysis), and in more depth via [Heroku](https://sentiment-analysis-twitter-bot.herokuapp.com/).
-
-## Getting started
-
-* Fork this repo and clone to your machine
-* Type ```npm i``` into the terminal to install required node packages
-
-### Running tests
-
-* Type ```jest``` into terminal to run the full test suite, or ```jest [file path]``` to run a specific test file.   
-
+# MA_final_project
 
 ## MVP user stories:
 > As a twitter user,  
@@ -26,24 +12,6 @@ This information is presented both via automated Language Reports tweeted out vi
 > As a twitter user,  
 > so I know what the top debates are,  
 > I want the twitter bot to inform me what they are.
-
-## Tech / framework used
-* __Javascript__
-* __CSS__
-* __Node.js__
-* __Jest__ (testing framework)
-* __Twitter API__
-* __Twitter apps__
-* __React__
-* __Heroku__
-
-* __Trello__ (kanban-style workflow manager)
-* __Balsamiq__ (design mock-up)
-
-## Development process and workflow 
-* __2 day sprints__ - at the start we discussed current progress, estimated the scope of new features (classing tasks as  small, medium or large), assessed blockages and assigned tasks amongst ourselves.
-* __Retros__ - at the end of each sprint to reflect on what had been achieved, what had gone well and what we could improve.
-* __Standups__ - a less formal chat at the start of the day where each person talks through work from the previous day, any issues and what they hope to achieve in the coming day.
 
 ## First retro
 
@@ -69,22 +37,5 @@ The second two day sprint focused on the following user stories:
 > As a group member  
 > So I know the API data is being handled correctly  
 > All the JS API functions are fully tested  
-
-## Second retro
-
-![Second retro](/assets/retro2.JPG "retro 2")
-
-We made some significant progress in the second sprint:  
-- Set up React so we could begin developing our front end
-- Reformatted the repository folder structure to work with React, including merging package.json files
-- Passed a full suite of API JS and React component tests 
-- Incorporated graphing functionality to represent data in the front end.
-
-## Middle weekend work
-
-![Weekend work](/assets/weekend_work.JPG "weekend-work") 
-
-Over the weekend we split up several tasks that could be done individually
-
 
 
--- spec/twitcall.test.js
+++ spec/twitcall.test.js
@@ -1,66 +1,67 @@
 const TwitCall = require('../src/TwitCall')
 
-  var mockTwitter;
-  var promisedData = [{trends: [{name: "#wednesdaywisdom"},
-  {name: "#wednesdaywisdom"},
-  {name: "#wednesdaywisdom"},
-  {name: "#wednesdaywisdom"},
-  {name: "#wednesdaywisdom"},
-  {name: "#wednesdaywisdom"},
-  {name: "#wednesdaywisdom"},
-  {name: "#wednesdaywisdom"},
-  {name: "#wednesdaywisdom"},
-  {name: "#wednesdaywisdom"},
-]}];
-  var data;
+var mockTwitter
+var promisedData = [{trends: [{name: '#wednesdaywisdom'},
+  {name: '#wednesdaywisdom'},
+  {name: '#wednesdaywisdom'},
+  {name: '#wednesdaywisdom'},
+  {name: '#wednesdaywisdom'},
+  {name: '#wednesdaywisdom'},
+  {name: '#wednesdaywisdom'},
+  {name: '#wednesdaywisdom'},
+  {name: '#wednesdaywisdom'},
+  {name: '#wednesdaywisdom'}
+]}]
+var data
+var twitcall
 
-  beforeEach(()=> {
-    mockTwitter = {
-      get: function(url, callback) {
-        callback(null, promisedData, null)
-      },
-      post: function(status, tweet, callback) {
-        callback(null, 'tweet', null)
-      }
+beforeEach(() => {
+  mockTwitter = {
+    get: function (url, callback) {
+      callback(null, promisedData, null)
+    },
+    post: function (status, tweet, callback) {
+      callback(null, 'tweet', null)
     }
-    twitcall = new TwitCall(mockTwitter);
+  }
+  twitcall = new TwitCall(mockTwitter)
+})
+
+describe('#getTrends', function () {
+  it('calls the Twitter API', function () {
+    spyOn(mockTwitter, 'get')
+    twitcall.getTrends()
+    expect(mockTwitter.get).toHaveBeenCalled()
   })
 
-  describe("#getTrends", function() {
-    it('calls the Twitter API', function() {
-      spyOn(mockTwitter, 'get')
-      twitcall.getTrends();
-      expect(mockTwitter.get).toHaveBeenCalled();
-    });
-
-    it('returns tweets in an array', function(done){
-      twitcall.getTrends().then(function(pData) {
-        expect(pData[0]).toEqual("#wednesdaywisdom")
-      });
-      done();
-    });
-
-  describe("#getTweets", function() {
-    it('it calls the twitter API', function(done) {
-      twitcall.getTweets().then(function(pData){
-        expect(pData[0]).toEqual("#wednesdaywisdom")
-      });
-      done();
-    });
-
-    it('calls the Twitter API', function() {
-      spyOn(mockTwitter, 'get')
-      twitcall.getTweets();
-      expect(mockTwitter.get).toHaveBeenCalled();
-    });
-  })
-
-  describe("#updateStatus", function() {
-    it('updates twitter status', function(done) {
-      twitcall.updateStatus().then(function(tweet) {
-        expect(tweet).toEqual("tweet")
-      });
-      done();
+  it('returns tweets in an array', function (done) {
+    twitcall.getTrends().then(function (pData) {
+      expect(pData[0]).toEqual('#wednesdaywisdom')
     })
-  })  
-});
+    done()
+  })
+
+  describe('#getTweets', function () {
+    it('it calls the twitter API', function (done) {
+      twitcall.getTweets().then(function (pData) {
+        expect(pData[0]).toEqual('#wednesdaywisdom')
+      })
+      done()
+    })
+
+    it('calls the Twitter API', function () {
+      spyOn(mockTwitter, 'get')
+      twitcall.getTweets()
+      expect(mockTwitter.get).toHaveBeenCalled()
+    })
+  })
+
+  describe('#updateStatus', function () {
+    it('updates twitter status', function (done) {
+      twitcall.updateStatus().then(function (tweet) {
+        expect(tweet).toEqual('tweet')
+      })
+      done()
+    })
+  })
+})
