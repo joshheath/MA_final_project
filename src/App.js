17a2c5bf0821dbfae1734e322b98517bed889d7d
Chris Russell <chris.john.russell@gmail.com>
2018-07-30 12:28:45 +0100

    Import NaturalLanguageCall

--- src/App.js
+++ src/App.js
@@ -2,44 +2,38 @@ import React, { Component } from 'react';
 import './App.css';
 import Hashtag from './components/hashtag.jsx';
 import { VictoryBar, VictoryChart, VictoryAxis } from 'victory';
+import * as NaturalLanguageCall from './NaturalLanguageCall';
 
 const data = [
-  {quarter: 1, earnings: 100},
-  {quarter: 2, earnings: 100},
-  {quarter: 3, earnings: 100},
-  {quarter: 4, earnings: 100}
+  {emotions: 1, index: 100},
+  {emotions: 2, index: 100},
+  {emotions: 3, index: 100},
+  {emotions: 4, index: 100},
+  {emotions: 5, index: 100}
 ];
 
 class App extends Component {
   render () {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div>
-          <Hashtag hashtags={['matt', 'wareing']}/>
-        </div>
+        <p>#Arnold Schwartznegger</p>
         <div>
         <div>
         <VictoryChart
         domainPadding={20}
         >
         <VictoryAxis
-        tickValues={[1, 2, 3, 4]}
-        tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
+        tickValues={[1, 2, 3, 4, 5]}
+        tickFormat={["Sadness", "Fear", "Anger", "Disgust", "Joy"]}
         />
         <VictoryAxis
         dependentAxis
-        tickFormat={(x) => (`${x / 100}`)}
+        tickFormat={(x) => (`${x / 1}`)}
         />
             <VictoryBar
             data={data}
-            x="quarter"
-            y="earnings" />
+            x="emotions"
+            y="index" />
             </VictoryChart>
             </div>
         </div>
