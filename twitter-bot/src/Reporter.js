88c996fafcc2e5b8e1cbd8d1097c515fe40cc691
Matthew <mdwareing@gmail.com>
2018-07-30 11:18:41 +0100

    Refactor file structure

--- twitter-bot/src/Reporter.js
+++
@@ -1,7 +0,0 @@
-function Reporter() {}
-
-Reporter.prototype.compile = function (trend, toneanalysis, naturallanguage) {
-  return(`LANGUAGE REPORT for ${trend}:\nTone: ${toneanalysis.tones[0]},\nPredominant sentiment: ${naturallanguage.sentiment}\nEmotion ratings:\nsadness: ${naturallanguage.emotions.sadness},\njoy: ${naturallanguage.emotions.joy},\nfear: ${naturallanguage.emotions.fear},\ndisgust: ${naturallanguage.emotions.disgust},\nanger: ${naturallanguage.emotions.anger}\nMain concept(s): ${naturallanguage.concepts[0]}, ${naturallanguage.concepts[1]}`)
-};
-
-module.exports = Reporter;
