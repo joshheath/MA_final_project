adecc819d8c973f71edd249d0ba396f62a1622ea
Nick <nicholasjohnrichardson@gmail.com>
2018-07-31 13:02:13 +0100

    graph displays api data correctly!

--- src/App.js
+++ src/App.js
@@ -6,23 +6,44 @@ import NaturalLanguageCall from './NaturalLanguageCall.js'
 
 const nlc = new NaturalLanguageCall();
 
-class Graph extends Component {
+
+class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      emotionData: []
     }
   }
+
   componentDidMount() {
-    nlc.analyzeLanguage("The letter was written by Zoe Carew who became incensed when she saw the “Linemen” signs – which indicate that people installing or fixing power lines are working in the area – while on her way to visit her grandparents in the city of Eastbourne.").then((data) => {
-      let componentData = data.emotions;
-      this.setState(componentData);
+    nlc.analyzeLanguage("The Saudi ministry of defence is run by His Royal Highness Prince Mohammed bin Salman bin Abdulaziz Al Saud. The 32-year-old, known as MbS, is said to be the world’s youngest defence minister and is also the kingdom’s deputy prime minister.").then((data) => {
+      let apiData = data.emotions;
+      let emotionData = [{emotion: 1, index: apiData.sadness}, {emotion: 2, index: apiData.fear}, {emotion: 3, index: apiData.anger}, {emotion: 4, index: apiData.disgust}, {emotion: 5, index: apiData.joy}]
+      this.setState({emotionData: emotionData});
+      console.log(this.state);
     });
+
   }
 
   render () {
-    console.log(this.state)
-    const analysis = [{emotion: 1, index: this.state.sadness}, {emotion: 2, index: this.state.fear}, {emotion: 3, index: this.state.anger}, {emotion: 4, index: this.state.disgust}, {emotion: 5, index: this.state.joy}]
+    console.log(this.state.emotionData);
+    return (
+      <div className="App">
+        <div className="Header">
+        <h1>Our Conversation</h1>
+        </div>
+        <Graph
+          emotionData={this.state.emotionData}
+        />
+      </div>
+    )
+  }
+}
+
+class Graph extends Component {
+
+  render () {
+    // console.log(this.state)
     return (
       <div className="Graph">
         <div>
@@ -39,7 +60,7 @@ class Graph extends Component {
         tickFormat={(x) => (`${x / 1}`)}
         />
             <VictoryBar
-            data={analysis}
+            data={this.props.emotionData}
             x="emotion"
             y="index" />
             </VictoryChart>
@@ -49,26 +70,5 @@ class Graph extends Component {
     )
   }
 }
-
-
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-  }
-
-  render () {
-    return (
-      <div className="App">
-        <div className="Header">
-        <h1>Our Conversation</h1>
-        </div>
-        <Graph />
-      </div>
-    )
-  }
-}
-
-
 
 export default App
