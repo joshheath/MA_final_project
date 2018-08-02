179ca8e830055550f119f088acbd290cfaaa6a04
Matthew <mdwareing@gmail.com>
2018-08-02 18:29:36 +0100

    Update es6

--- src/Reporter.js
+++ src/Reporter.js
@@ -1,15 +1,15 @@
-function Reporter() {}
+class Reporter {
+  compile(trend, toneanalysis, naturallanguage) {
+    return(`LANGUAGE REPORT for ${trend}:\nTone: ${toneanalysis},\nPredominant sentiment: ${naturallanguage.sentiment}\nEmotion ratings:\nsadness: ${naturallanguage.emotions.sadness},\njoy: ${naturallanguage.emotions.joy},\nfear: ${naturallanguage.emotions.fear},\ndisgust: ${naturallanguage.emotions.disgust},\nanger: ${naturallanguage.emotions.anger}\nMain concept(s): ${naturallanguage.concepts}`)
+  }
+}
 
-Reporter.prototype.compile = function (trend, toneanalysis, naturallanguage) {
-  return(`LANGUAGE REPORT for ${trend}:\nTone: ${toneanalysis},\nPredominant sentiment: ${naturallanguage.sentiment}\nEmotion ratings:\nsadness: ${naturallanguage.emotions.sadness},\njoy: ${naturallanguage.emotions.joy},\nfear: ${naturallanguage.emotions.fear},\ndisgust: ${naturallanguage.emotions.disgust},\nanger: ${naturallanguage.emotions.anger}\nMain concept(s): ${naturallanguage.concepts}`)
-};
+const reporter = new Reporter()
 
-const reporter = new Reporter();
-
-var trend = "#Ibiza"
-var toneanalysis = "positive"
-var naturallanguage = {sentiment: 'neutral', emotions: {sadness: 1, joy: 2, fear: 3, disgust: 4, anger: 5}, concepts: ['Holidays']}
+const trend = "#Ibiza";
+const toneanalysis = "positive";
+const naturallanguage = {sentiment: 'neutral', emotions: {sadness: 1, joy: 2, fear: 3, disgust: 4, anger: 5}, concepts: ['Holidays']}
 
 reporter.compile(trend, toneanalysis, naturallanguage);
 
-module.exports = Reporter;
+export default Reporter
